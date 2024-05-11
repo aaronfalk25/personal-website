0b98a97164acd1400568701c61f00a886cf10114
Aaron Falk <aaronfalk25@gmail.com>
2024-05-10 23:12:22 -0400

    rip about section from linkedin bio

--- src/app/components/About/index.tsx
+++ src/app/components/About/index.tsx
@@ -6,7 +6,9 @@ export const About: React.FC = () => {
         <Element name="about">
             <h1>About</h1>
             <p>
-                This is a Next.js app with TypeScript, ESLint, Prettier, and Tailwind CSS.
+            I view the field of software development as the most artistic form of problem-solving in modern engineering. The crux of computer science is taking on a new problem and delivering a solution that is elegant, precise, and carefully crafted. Ever since I started coding in middle school, I have been deepening my understanding of how to leverage modern technologies and skills to interface with the technical world around us.
+
+            As I am studying my coursework at Penn State, I am learning how to take the rigor of computer science and marry it with my imagination and creativity. My goal is to continually transform my understanding and abilities by taking on new projects and never being afraid to ask questions. I hope to inspire others and progress my view of technology.
             </p>
         </Element>
     );
